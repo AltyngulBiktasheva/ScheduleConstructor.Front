3eeb6c31d38d8c6f623648b3f3041fc9609b6c50
biktasheva <biktasheva@skbkontur.ru>
2026-02-24 16:34:15 +0500

    Создание vite проекта

---
+++ src/MainContainer/MainContainer.tsx
@@ -0,0 +1,11 @@
+import type {FC} from "react";
+
+
+export const MainContainer: FC = () => {
+    return (
+        <div>
+            <h1>MAIN CONTAINER</h1>
+            <p>Here we go again...</p>
+        </div>
+    )
+};
